7db73a4daee332cbd2e1d125a921a9a1dff7e575
xwangm0815 <90065800+xwangm0815@users.noreply.github.com>
2024-09-27 14:11:14 -0700

    Update example_astronauts.py
    Schedule to None

--- dags/example_astronauts.py
+++ dags/example_astronauts.py
@@ -1,24 +1,3 @@
-"""
-## Astronaut ETL example DAG
-
-This DAG queries the list of astronauts currently in space from the 
-Open Notify API and prints each astronaut's name and flying craft.
-
-There are two tasks, one to get the data from the API and save the results,
-and another to print the results. Both tasks are written in Python using
-Airflow's TaskFlow API, which allows you to easily turn Python functions into
-Airflow tasks, and automatically infer dependencies and pass data.
-
-The second task uses dynamic task mapping to create a copy of the task for
-each Astronaut in the list retrieved from the API. This list will change
-depending on how many Astronauts are in space, and the DAG will adjust 
-accordingly each time it runs.
-
-For more explanation and getting started instructions, see our Write your 
-first DAG tutorial: https://docs.astronomer.io/learn/get-started-with-airflow
-
-![Picture of the ISS](https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2010/02/space_station_over_earth/10293696-3-eng-GB/Space_Station_over_Earth_card_full.jpg)
-"""
 
 from airflow import Dataset
 from airflow.decorators import dag, task
@@ -28,7 +7,7 @@ import requests
 #Define the basic parameters of the DAG, like schedule and start_date
 @dag(
     start_date=datetime(2024, 1, 1),
-    schedule="@daily",
+    schedule=None,
     catchup=False,
     doc_md=__doc__,
     default_args={"owner": "Astro", "retries": 3},
